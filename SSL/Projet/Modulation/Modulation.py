b3a5872d90cb1ed7b0ed7bb319c5fe9dc8d0a5f0
MessengerYan <jmaxag@gmail.com>
2024-12-10 11:33:05 +0100

    transformée de fourier

--- SSL/Projet/Modulation/Modulation.py
+++ SSL/Projet/Modulation/Modulation.py
@@ -9,11 +9,12 @@ def porte(t,T):
     y=(abs(t)<T/2)
     return np.float16(y)
 
-rdic = io.loadmat('signal.mat')
+rdic = io.loadmat('SSL/Projet/Modulation/signal.mat')
 signal = rdic['signal'].squeeze()
 Fs = 96000
 Ts = 1/Fs
 t=[]
+
 for i in range(len(signal)):
     t.append(0+i*Ts)
 
@@ -21,5 +22,14 @@ df = pd.DataFrame({'temps':t, 'signal':signal})
 fig = px.line(df, x='temps', y='signal')
 fig.update_layout(xaxis_title='temps', yaxis_title='signal',
 title = 'Tracé du signal',
+template='plotly_white', width=500, height=300)
+fig.show()
+
+Trans, Nu = msi.TransFourier(signal, t)
+
+df = pd.DataFrame({'fréquence':Nu, 'transformée':np.real(Trans)})
+fig = px.line(df, x='fréquence', y='transformée')
+fig.update_layout(xaxis_title='fréquence', yaxis_title='transfomée',
+title = 'Tracé de la transformée de Fourier du signal',
 template='plotly_white', width=500, height=300)
 fig.show()

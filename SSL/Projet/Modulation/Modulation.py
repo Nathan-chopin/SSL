fc14b4027c88aceb9f7747659de8de5ef2ed8edc
MessengerYan <jmaxag@gmail.com>
2024-12-10 12:00:24 +0100

    signal modulé

--- SSL/Projet/Modulation/Modulation.py
+++ SSL/Projet/Modulation/Modulation.py
@@ -33,3 +33,45 @@ fig.update_layout(xaxis_title='fréquence', yaxis_title='transfomée',
 title = 'Tracé de la transformée de Fourier du signal',
 template='plotly_white', width=500, height=300)
 fig.show()
+
+sig_filtre= msi.PasseBas(signal, 96000, 3000)
+
+df = pd.DataFrame({'temps':t, 'signal':sig_filtre})
+fig = px.line(df, x='temps', y='signal')
+fig.update_layout(xaxis_title='temps', yaxis_title='signal',
+title = 'Tracé du signal filtré',
+template='plotly_white', width=500, height=300)
+fig.show()
+
+Trans_f, Nu = msi.TransFourier(sig_filtre, t)
+
+df = pd.DataFrame({'fréquence':Nu, 'transformée':np.real(Trans_f)})
+fig = px.line(df, x='fréquence', y='transformée')
+fig.update_layout(xaxis_title='fréquence', yaxis_title='transfomée',
+title = 'Tracé de la transformée de Fourier du signal filtré',
+template='plotly_white', width=500, height=300)
+fig.show()
+
+sig_mod = []
+for i in range(len(t)):
+    sig_mod.append(sig_filtre[i] * np.cos(2 * np.pi * 21000 * t[i]))
+
+df = pd.DataFrame({'temps':t, 'signal':sig_mod})
+fig = px.line(df, x='temps', y='signal')
+fig.update_layout(xaxis_title='temps', yaxis_title='signal',
+title = 'Tracé du signal modulé',
+template='plotly_white', width=500, height=300)
+fig.show()
+
+Trans_mod, Nu = msi.TransFourier(sig_mod, t)
+
+df = pd.DataFrame({'fréquence':Nu, 'transformée':np.real(Trans_mod)})
+fig = px.line(df, x='fréquence', y='transformée')
+fig.update_layout(xaxis_title='fréquence', yaxis_title='transfomée',
+title = 'Tracé de la transformée de Fourier du signal modulé',
+template='plotly_white', width=500, height=300)
+fig.show()
+
+mdic = {"sig_mod_fp_21kHz": sig_mod}
+
+io.savemat('sig_mod_fp_21kHz.mat', mdict=mdic)

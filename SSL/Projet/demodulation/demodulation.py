4bbdef83189926fd59777ad8e5cfb927f5465a07
Mael <mael.pierron@cpe.fr>
2024-12-10 10:00:23 +0100

    ouverture fichier

--- SSL/Projet/demodulation/demodulation.py
+++ SSL/Projet/demodulation/demodulation.py
@@ -2,7 +2,12 @@ import numpy as np
 import pandas as pd
 import plotly.express as px
 import msicpe.ssl as msi
+import scipy.io as io
 
 def porte(t, T):
     y=(abs(t)<T/2)
     return np.float16(y)
+
+
+rdic = io.loadmat('signal_multiplex.mat')
+signal_multiplex = rdic['melange'].squeeze()
